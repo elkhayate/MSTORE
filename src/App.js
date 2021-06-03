cfad90ddfc5a2ed186e6882ab650e197605d3bde
elkhayate <melkhayate61@gmail.com>
2021-06-03 05:58:09 +0300

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import React from 'react';
+import info from "./components/info";
+import style from "./app.module.css";
+import Nav from "./Nav";
 function App() {
   return (
-    <div>
+    <Router>
+    <div className={style.app}>
 
+        <Nav />
+     
+      <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path = "/info" component={info} />
+      </Switch>
     </div>
+    </Router>
   );
 }
+
+function Home() {
+  return(
+    <div>
+      <h1>momo</h1>
+    </div>
+  )
+}
+
 
 export default App;
---
+++ src/components/info.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+
+function Info() {
+    return(
+        <div>
+            <h1>popo</h1>
+        </div>
+    )
+}
+
+
+export default Info;
